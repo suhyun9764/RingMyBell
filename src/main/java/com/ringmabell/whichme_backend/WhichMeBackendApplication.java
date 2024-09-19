19d57e2395a41bd0656cabae9b9466c8e363bc0a
suhyun9764 <suhyun9764@naver.com>
2024-09-19 18:44:13 +0900

    Initial commit

---
+++ src/main/java/com/ringmabell/whichme_backend/WhichMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ringmabell.whichme_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhichMeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhichMeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ringmabell/whichme_backend/WhichMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ringmabell.whichme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhichMeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
