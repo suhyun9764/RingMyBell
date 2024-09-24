344c3d2112b557557102662cd384d4a509c1d773
suhyun9764 <suhyun9764@naver.com>
2024-09-25 04:27:24 +0900

    feat: existByUsername

--- src/main/java/com/ringmabell/whichme_backend/repository/UserRepository.java
+++ src/main/java/com/ringmabell/whichme_backend/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.ringmabell.whichme_backend.entitiy.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
 }
