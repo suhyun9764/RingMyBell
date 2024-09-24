73598c3e341c4930a57f35972b69f68cd8eafaa4
suhyun9764 <suhyun9764@naver.com>
2024-09-25 04:31:54 +0900

    style: Add space

--- src/main/java/com/ringmabell/whichme_backend/entitiy/User.java
+++ src/main/java/com/ringmabell/whichme_backend/entitiy/User.java
@@ -21,20 +21,27 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String username;
+
 	@Column(nullable = false)
 	private String password;
+
 	@Column(nullable = false)
 	private String realName;
-	@Column(nullable = false)
+
+	@Column(nullable = false, unique = true)
 	private String email;
+
 	@Column(nullable = false)
 	private String phone;
+
 	@Column(nullable = false)
 	private String address;
+
 	@Column(nullable = false)
 	private String provider;
+
 	private String providerId;
 
 }
