4f3357b1731263b9e96578c6c71b75daa64062f3
suhyun9764 <suhyun9764@naver.com>
2024-09-28 21:05:28 +0900

    feat(): add login, join related message

--- src/main/java/com/ringmabell/whichme_backend/constants/UserMessages.java
+++ src/main/java/com/ringmabell/whichme_backend/constants/UserMessages.java
@@ -11,4 +11,8 @@ public class UserMessages {
     public static final String ALREADY_EXIST_EMAIL = "이미 존재하는 이메일 입니다";
     public static final String INVALID_USERNAME = "일치하는 계정이 없습니다";
     public static final String INVALID_PASSWORD = "비밀번호 오류입니다";
-}
+    public static final String AVAILABLE_USERNAME = "사용가능한 아이디입니다";
+    public static final String AVAILABLE_EMAIL = "사용가능한 이메일입니다";
+    public static final String COMPLETE_JOIN = "회원가입이 완료되었습니다";
+    public static final String COMPLETE_LOGIN = "로그인 성공";
+ }
