706f1751faa9369428b9655d391ed4cae93b9546
suhyun9764 <suhyun9764@naver.com>
2024-10-03 01:05:29 +0900

    feat(): create DispatchMessage

---
+++ src/main/java/com/ringmabell/whichme_backend/constants/DispatchMessage.java
@@ -0,0 +1,7 @@
+package com.ringmabell.whichme_backend.constants;
+
+public class DispatchMessage {
+	public static final String COMPLETE_VEHICLE_JOIN = "차량등록이 완료되었습니다";
+	public static final String ALREADY_EXIST_VEHICLE = "이미 등록된 차량입니다";
+	public static final String NOT_EXIST_SUBUNIT = "존재하지 않는 부서입니다";
+}
