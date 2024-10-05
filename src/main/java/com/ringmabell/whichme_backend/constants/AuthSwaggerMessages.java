002b7e24be568bbf0b66fd7434ac488ad0484923
suhyun9764 <suhyun9764@naver.com>
2024-10-05 23:55:04 +0900

    feat(): add auth controller for swagger

---
+++ src/main/java/com/ringmabell/whichme_backend/constants/AuthSwaggerMessages.java
@@ -0,0 +1,7 @@
+package com.ringmabell.whichme_backend.constants;
+
+public class AuthSwaggerMessages {
+	public static final String LOGIN_COMPLETE_EXAMPLE = "{ \"success\": true, \"message\": \"로그인 성공\" }";
+	public static final String USERNAME_ERROR_EXAMPLE = "{ \"success\": false, \"message\": \"일치하는 계정이 없습니다\" }";
+	public static final String PASSWORD_ERROR_EXAMPLE = "{ \"success\": false, \"message\": \"비밀번호 오류입니다\" }";
+}
