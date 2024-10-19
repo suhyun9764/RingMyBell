59d5466f4060cf1f87ba2ed1bdc574347d619707
suhyun9764 <suhyun9764@naver.com>
2024-10-19 18:55:24 +0900

    refactor: cors 접근 경로 변경

--- src/main/java/com/ringmabell/whichme_backend/config/SecurityConfig.java
+++ src/main/java/com/ringmabell/whichme_backend/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 							public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 								CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-								corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+								corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
 								corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
 								corsConfiguration.setAllowCredentials(true);
 								corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
